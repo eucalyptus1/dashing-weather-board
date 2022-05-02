2329c1b0ea8b35430f3eda1eb443ed18801f97c5
euc1 <jamesharrington585@hotmail.com>
2022-05-02 02:54:00 -0400

    got the image working. that's progress. working on uv index and 5 day forecast

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# dashing-weather-board
+# Weather Dashboard
+
+## Purpose
+A weather app that can give the current, and 5 day forecast of any city, showing the temperature, wind speed, UV index and humidity. It stores each entry so they can be easily referenced again if need be.
+
+## Directions
+Enter the name of a city into the input form and submit. It will display the current weather stats for that particular city, and underneath, show the upcoming 5 day forecast.
+Previously entered cities and be viewed by clicking the stored links below the form.
+
+## Built with
+* HTML
+* ~~CSS~~
+* JavaScript
+* Weather app API was provided by https://openweathermap.org/
+
+## Made By
+James
+
+## Website
+https://eucalyptus1.github.io/dashing-weather-board/
+
+![screenshot](./scrnsht.png)
--- script.js
+++ script.js
@@ -1,47 +1,39 @@
-var locationElement = document.querySelector(".city p");
-var dateElement = document.querySelector(".date p");
-var iconElement = document.querySelector(".image");
-var tempElement = document.querySelector(".temp p");
-var descElement = document.querySelector(".desc p");
-var windElement = document.querySelector(".wind p");
-var humElement = document.querySelector(".humidity p");
-var uvElement = document.querySelector(".uv p");
+var city = document.getElementById("#city");
+var date = document.getElementById("#date");
+var icon = document.getElementById("#image");
+var temp = document.getElementById("#temp");
+var desc = document.getElementById("#desc");
+var wind = document.getElementById("#wind");
+var humidity = document.getElementById("#humidity");
+var uv = document.getElementById("#uv");
 
-var citySubmit = document.querySelector("#city-submit");
+var dt = $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+
+var btn = document.getElementById("#btn");
+var cityInput = document.getElementById("#search");
 
 
 var key = "7f9364bc4336da280bffe6dfb0be17b2";
-
-const weather = {};
-
+cityArr = []
 
 
 
-//geomapping for getting users locations
-// navigator.geolocation.getCurrentPosition(setPosition);
 
-
-// function setPosition(position){
-//     let lat = position.coords.latitude;
-//     let lon = position.coords.longitude;
-    
-//     getWeather(lat, lon);
-// };
-
-var searchCity = function(event) {
+function searchCity(event) {
     event.preventDefault();
-    var selectedCity = document.querySelector("#search-input").value;
+    var selectedCity = cityInput.value;
     
-    //console.logs for testing
     console.log(selectedCity);
-  
-    // if (!selectedCity) {
-    //   alert("You need to enter a city");
-    //   return false;
-    // } else {
-    //  
+
+    if (!selectedCity) {
+      alert("You need to enter a city");
+      return false;
+    } else {
+        cityArray.push(selectedCity);
+        localStorage.setItem(selectedCity, JSON.stringify(cityArr));
+     
       
-    // }
+    }
   
     getWeather(selectedCity);
   };
@@ -50,40 +42,56 @@ var searchCity = function(event) {
 
     function getWeather(selectedCity){   
         let api = `https://api.openweathermap.org/data/2.5/weather?q=${selectedCity}&units=imperial&appid=${key}`;
-    fetch(api)
+        fetch(api)
         .then(function(response){
             let data = response.json();
             console.log(data);
             return data;
-            
         })
         .then(function(data){
-            weather.temperature = data.main.temp;
-            weather.date = data.dt;
-            weather.description = data.weather[0].description;
-            weather.iconId = data.weather[0].icon;
-            weather.city = data.name;
-            weather.country = data.sys.country;
-            weather.humidity = data.humidity;
-            weather.uv = data.uvi;
-            weather.wind = data.wind.speed;
+            var iconId = data.weather[0].icon;
+            city.innerHTML = data.name + ", " + data.sys.country;
+            icon.innerHTML = `<img src="https://openweathermap.org/img/wn/${iconId}@4x.png"/>`;
+            desc.innerHTML = data.weather[0].description;
+            var lat = data.coord.lat;
+            var lon = data.coord.lon;
+        let apiTwo = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${key}`;
+        fetch(apiTwo)
+        .then(function(response){
+            let data = response.json();
+            console.log(data);
+            return data;
         })
-        .then(function(){
-            displayWeather();
-        });
+        .then(function(data){
+           date.innerHTML = dt
+           temp.innerHTML = data.current.temp;
+           wind.innerHTML = data.current.wind_speed;
+           humidity.innerHTML = data.current.humidity;
+           uv.innerHTML = data.uvi;
+        })
+        // .then(function(){
+        //     displayWeather();
+        // });
+
+        // function fiveDay(lat, lon){
+        })
+
+        
     }
 
 // display function
-function displayWeather(){
-    locationElement.innerHTML = `${weather.city},${weather.country}`;
-    dateElement.innerHTML = `${weather.date}`;
-    iconElement.innerHTML = `<img src="https://openweathermap.org/img/wn/${weather.iconId}@4x.png"/>`;
-    tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
-    descElement.innerHTML = `${weather.description}`;
-    windElement.innerHTML = `${weather.wind}`;
-    humElement.innerHTML = `${weather.humidity}`;
-    uvElement.innerHTML = `${weather.uv}`;
-    }
+// function displayWeather(){
+//     locationElement.innerHTML = `${weather.city},${weather.country}`;
+//     dateElement.innerHTML = `${weather.date}`;
+//     iconElement.innerHTML = `<img src="https://openweathermap.org/img/wn/${weather.iconId}@4x.png"/>`;
+//     tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+//     descElement.innerHTML = `${weather.description}`;
+//     windElement.innerHTML = `${weather.wind}`;
+//     humElement.innerHTML = `${weather.humidity}`;
+//     uvElement.innerHTML = `${weather.uv}`;
+//     }
 
 
-    citySubmit.addEventListener("click", searchCity);
+
+
+btn.addEventListener("click", searchCity);
